ba4c7fe21fd7a8eeee0ff23eddf612b197bae716
GrishaBony <gr_ichet@mail.ru>
2025-05-10 10:58:53 +0400

    add /newchat page

---
+++ pages/newChat.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'chat-list-layout',
+});
+</script>
+
+<template>
+    <div class="p-4 sm:p-6 space-y-4 sm:space-y-6">
+      newchat
+    </div>
+</template>
